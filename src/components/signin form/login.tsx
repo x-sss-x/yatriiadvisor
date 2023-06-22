af2d7fa26b45d32ff647161e64bd4d43295d0fcb
= <=>
2023-06-22 22:07:29 +0530

    added

--- src/components/signin form/login.tsx
+++ src/components/signin form/login.tsx
@@ -1,12 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import LOG_button from "./LOG_button";
-import For_Button from "./forgot_pwd_button";
-import { FcGoogle } from "react-icons/fc";
-import { FaFacebook, FaUserAlt } from "react-icons/fa";
-
 import Link from "next/link";
-import Signup_button from "./signin";
 
 const Login = () => {
   const [username, setUsername] = useState("");
@@ -17,21 +12,25 @@ const Login = () => {
   };
 
   return (
-    <div className="pt-2 pb-8" >
+    <div className="pt-2 pb-8">
       <div
         className="font-bold text-button font-roboto text-center pt-2 pb-2"
-        style={{ fontSize: 20}}
+        style={{ fontSize: 20 }}
       >
         <h1>Login</h1>
       </div>
-      <form  style={{ fontSize: 10 }}>
-        <div className="font-bold text-button font-roboto flex pt-2 pb-2 container flex pl-4" style={{ fontSize: 14 }}>
+      <form style={{ fontSize: 10 }}>
+        <div
+          className="font-bold text-button font-roboto flex pt-2 pb-2 container flex pl-4"
+          style={{ fontSize: 14 }}
+        >
           <label htmlFor="username">Username:</label>
           <br />
         </div>
-        <div className="pl-4" >
+        <div className="pl-4">
           <input
-            className="rounded-full pl-20 pr-20 pb-2 pt-2 border-b border-button border-t shadow-lg" style={{ fontSize: 14 }}
+            className="rounded-full pl-20 pr-20 pb-2 pt-2 border-b border-button border-t shadow-lg"
+            style={{ fontSize: 14 }}
             type="text"
             placeholder="Name"
             id="username"
@@ -39,13 +38,15 @@ const Login = () => {
             onChange={(e) => setUsername(e.target.value)}
           />
         </div>
-        <div className="font-bold text-button font-roboto pt-4 pb-2 pl-4" style={{ fontSize: 14 }}>
+        <div
+          className="font-bold text-button font-roboto pt-4 pb-2 pl-4"
+          style={{ fontSize: 14 }}
+        >
           <label htmlFor="password">Password:</label>
           <br />
         </div>
         <div className="pl-4">
-        <input
-        
+          <input
             className="rounded-full pl-20 pr-20 pb-2 pt-2  border-b border-button border-t shadow-lg"
             style={{ fontSize: 14 }}
             type="password"
@@ -56,45 +57,16 @@ const Login = () => {
           />
         </div>
         <div className="pl-4">
-        <button
-      
-          type="submit"
-          className="pl-15 pr-15 pt-6 container  "
-          onClick={handleLogin}
-        >
-          <LOG_button />
-          <br />
-          <div className="flex pt-2 pb-2">
-            <For_Button />
-          </div>
-        </button></div>
-        <div
-          className="container flex justify-center text-button"
-          style={{ fontSize: 25 }}
-        >
-          ------------ or ------------
-        </div>
-        <div className="container pl-24">
-          <button>
-            <FcGoogle
-              className=" container margin-10px"
-              style={{ fontSize: 25 }}
-            />
-            <span className="container">Google</span>
-          </button>
-          <button>
-            <FaFacebook className="pl-20 container" style={{ fontSize: 25 }} />
-            <span className="pl-20 container ">Facebook</span>
-          </button>
-          <span
-            className=" container flex justify-center pt-6 pr-12 font-roboto font-bold text-button "
-            style={{ fontSize: 14 }}
+          <button
+            type="submit"
+            className="pl-15 pr-15 pt-6 container  "
+            onClick={handleLogin}
           >
-            {"Don't Have An Account ?"}
-            <Link href="../Signuppage"> <button type="submit"  onClick={handleLogin}>
-              <Signup_button/>
-            </button></Link>
-          </span>
+            <Link href={"/"}>
+              <LOG_button />
+            </Link>
+            <br />
+          </button>
         </div>
       </form>
     </div>
